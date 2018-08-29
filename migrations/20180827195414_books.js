dfe201725adbddd58c77db1504b14cf54a880180
Benjamin Austin <bnjmn80@gmail.com>
2018-08-29 14:36:09 -0600

    Changed data type in books migration file for foreign keys from string to integer

--- migrations/20180827195414_books.js
+++ migrations/20180827195414_books.js
@@ -6,9 +6,9 @@ exports.up = function(knex, Promise) {
         table.string('book_genre'); 
         table.string('book_description', 1000);
         table.string('book_cover_url');
-        table.string('author_id_one');
-        table.string('author_id_two');
-        table.string('author_id_three');
+        table.integer('author_id_one');
+        table.integer('author_id_two');
+        table.integer('author_id_three');
     });
 };
 
